3c94362b1406ef6253ad065125ca2e903b1d6cd7
Avengerandy007 <157376984+Avengerandy007@users.noreply.github.com>
2025-10-04 18:02:29 +0300

    Created entity class

---
+++ include/entities.hpp
@@ -0,0 +1,8 @@
+#include "util/vectors.hpp"
+class Entity2D{
+	Vector2 position;
+};
+
+class Entity3D{
+	Vector3 position;
+};
---
+++ include/util/vectors.hpp
@@ -0,0 +1,10 @@
+struct Vector2{
+	float X;
+	float Y;
+};
+
+struct Vector3{
+	float X;
+	float Y;
+	float Z;
+};

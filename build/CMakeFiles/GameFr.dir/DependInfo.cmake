976850f20d486b871d6615068376a64a68628e48
Avengerandy007 <157376984+Avengerandy007@users.noreply.github.com>
2025-10-04 17:37:37 +0300

    Created build system

---
+++ build/CMakeFiles/GameFr.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/andrei/Projects/GameFr/src/hey.cpp" "CMakeFiles/GameFr.dir/src/hey.cpp.o" "gcc" "CMakeFiles/GameFr.dir/src/hey.cpp.o.d"
+  "/home/andrei/Projects/GameFr/src/main.cpp" "CMakeFiles/GameFr.dir/src/main.cpp.o" "gcc" "CMakeFiles/GameFr.dir/src/main.cpp.o.d"
+  "" "GameFr" "gcc" "CMakeFiles/GameFr.dir/link.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/GameFr.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/GameFr.dir/link.d"
+  "CMakeFiles/GameFr.dir/src/hey.cpp.o"
+  "CMakeFiles/GameFr.dir/src/hey.cpp.o.d"
+  "CMakeFiles/GameFr.dir/src/main.cpp.o"
+  "CMakeFiles/GameFr.dir/src/main.cpp.o.d"
+  "GameFr"
+  "GameFr.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/GameFr.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

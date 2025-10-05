72715e19a2cc6dcb27a894488c405f9c90fd8b82
Avengerandy007 <157376984+Avengerandy007@users.noreply.github.com>
2025-10-05 14:58:30 +0300

    Added rectangular collision detection

--- include/entities.hpp
+++ include/entities.hpp
@@ -1,8 +1,29 @@
 #include "util/vectors.hpp"
+#include <inttypes.h>
 class Entity2D{
 public:
 	Vector2 position;
+	uint32_t width, height;
 	void Push(const Vector2& direction, const float& speed);
+	bool CollidingRect(const Entity2D& other) const;
+	virtual void Update() = 0;
+
+protected:
+	/*
+	 *	Make sure to put this in an update function so that the renderer knows where to render the object
+	 *	Call it with the arguments of the rect that your rendering library is using
+	 *	And the template argument that represents the type that the rect uses
+	 *	Ex: 
+	 *		Raylib: 
+	 *		AssignDimensionsToRenderingLibRect<float>(rect.x, rect.y, rect.w, rect.h);
+	 */
+	template<typename T>
+	void AssignDimensionsToRenderingLibRect(T& X, T& Y, T& W, T& H){
+		X = position.X;
+		Y = position.Y;
+		W = width;
+		H = height;
+	}
 };
 
 class Entity3D{
--- src/entities.cpp
+++ src/entities.cpp
@@ -4,3 +4,30 @@ void Entity2D::Push(const Vector2& direction, const float& speed){
 	position.X += direction.X * speed;
 	position.Y += direction.Y * speed;
 }
+
+
+bool Entity2D::CollidingRect(const Entity2D& other) const {
+    int leftA   = position.X;
+    int rightA  = position.X + width;
+    int topA    = position.Y;
+    int bottomA = position.Y + height;
+
+    int leftB   = other.position.X;
+    int rightB  = other.position.X + other.width;
+    int topB    = other.position.Y;
+    int bottomB = other.position.Y + other.height;
+
+    // If one rectangle is to the left of the other -> no collision
+    if (rightA < leftB)  return false;
+    if (rightB < leftA)  return false;
+
+    // If one rectangle is above the other -> no collision
+    if (bottomA < topB)   return false;
+    if (bottomB < topA)   return false;
+
+    // Otherwise there is an intersection
+    return true;
+}
+
+
+

b1e187d651ca76d0bd277eae3c506594c809c5a1
Avengerandy007 <157376984+Avengerandy007@users.noreply.github.com>
2025-10-24 21:30:29 +0300

    Added header guards

--- include/util/Randomizer.hpp
+++ include/util/Randomizer.hpp
@@ -1,10 +1,11 @@
+#pragma once
 #include <random>
 
 namespace GameFr{
 	class Randomizer{
 		std::random_device rd;
 		std::mt19937 gen;
-		std::uniform_int_distribution<> distrib;
+		std::uniform_int_distribution<int> distrib;
 	public:
 		Randomizer(int min, int max);
 		void ChangeRange(int min, int max);

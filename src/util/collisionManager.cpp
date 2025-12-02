d7489cc42de2fc9e0b4fb2da937581043bee47d5
Avengerandy007 <157376984+Avengerandy007@users.noreply.github.com>
2025-12-02 22:06:45 +0200

    Added pythagorean theorem function

--- src/util/collisionManager.cpp
+++ src/util/collisionManager.cpp
@@ -1,11 +1,12 @@
 #include "util/collisionManager.hpp"
+#include "GFmath.hpp"
 
 bool GameFr::Util::CollisionManager::CheckCollision(const std::shared_ptr<Entity2D> first, const std::shared_ptr<Entity2D> second){
 	if (useAABB){
 		if (first->CollidingRect(*second)) return true;
 		return false;
 	}
-	if (first->CollidingCircle(*second, 50)) return true;
+	if (first->CollidingCircle(*second, GameFr::Util::Math::Pythagorean(first->width, first->height) / 2)) return true;
 	return false;
 }
 
---
+++ src/util/math.cpp
@@ -0,0 +1,5 @@
+#include "util/GFmath.hpp"
+
+float GameFr::Util::Math::Pythagorean(const float a, const float b){
+	return std::sqrt(a * a + b * b);
+}

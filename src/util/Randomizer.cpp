b27f4eba43045a1cb2a7fceddf971a9aef02b9dc
Avengerandy007 <157376984+Avengerandy007@users.noreply.github.com>
2025-12-05 21:51:23 +0200

    Improved Randomizer optimization

--- src/util/Randomizer.cpp
+++ src/util/Randomizer.cpp
@@ -1,18 +1,25 @@
 #include "util/Randomizer.hpp"
+#include <cassert>
+#include <random>
 
 namespace GameFr{
 	Randomizer::Randomizer(int min, int max){
 		gen.seed(rd());
-		distrib = std::uniform_int_distribution(min, max);
+		minimum = min;
+		maximum = max;
 	}
 	Randomizer::Randomizer(const Randomizer& other){
 		gen = other.gen;
-		distrib = other.distrib;
+		minimum = other.minimum;
+		maximum = other.maximum;
 	}
 	void Randomizer::ChangeRange(int min, int max){
-		distrib = std::uniform_int_distribution(min, max);
+		minimum = min;
+		maximum = max;
 	}
 	int Randomizer::GetRandomNumber(){
+		assert(minimum <= maximum);
+		std::uniform_int_distribution<int> distrib(minimum, maximum);
 		return distrib(gen);
 	}
 }

13b488b5ed132ef994007acffefe13f99d9f43f6
Avengerandy007 <157376984+Avengerandy007@users.noreply.github.com>
2025-12-27 19:10:48 +0200

    Possibly fixed issue with event system It was re-returning the entire queue in some cases

--- src/event.cpp
+++ src/event.cpp
@@ -11,7 +11,7 @@ Gf::Event::Event(const Types t, const std::shared_ptr<const Entity2D> s, const s
 
 void Gf::EventQueue::CreateEvent(const std::shared_ptr<const Event>& event){
 	assert(event);
-	if (qp >= 10000){
+	if (qp > 9999){
 		qp = 0;
 	}
 	queue[qp] = event;
@@ -21,17 +21,17 @@ void Gf::EventQueue::CreateEvent(const std::shared_ptr<const Event>& event){
 
 std::shared_ptr<const Gf::Event> Gf::EventInterface::Listen(const std::shared_ptr<const Entity2D> parent){
 	assert(queue);
-	qp = (qp < 9999) ? qp : 0;
-	uint32_t limit = (qp <= queue->qp && queue->qp < 10000) ? queue->qp : 10000;
+	qp = (qp < 10000) ? qp : 0;
+	uint32_t limit = (qp <= queue->qp) ? queue->qp : 10000;
 	for(; qp < limit ; qp++){
-		if (qp > 9999 && queue->qp < 9999){
-			limit = queue->qp;
-			qp = 0;
-		}
 		if (!queue->queue[qp]) break;
 		if (queue->queue[qp]->receiver == parent) {
 			qp++;
 			return queue->queue[qp - 1];
+		}
+		if (qp >= 9999){
+			limit = (queue->qp != 0) ? queue->qp : limit;
+			qp = 0;
 		}
 	}
 	return nullptr;
@@ -39,17 +39,17 @@ std::shared_ptr<const Gf::Event> Gf::EventInterface::Listen(const std::shared_pt
 
 std::shared_ptr<const Gf::Event> Gf::EventInterface::Listen(const Gf::Event::Types desiredType){
 	assert(queue);
-	qp = (qp < 9999) ? qp : 0;
-	uint32_t limit = (qp <= queue->qp && queue->qp < 10000) ? queue->qp : 10000;
+	qp = (qp < 10000) ? qp : 0;
+	uint32_t limit = (qp <= queue->qp) ? queue->qp : 10000;
 	for(; qp < limit ; qp++){
-		if (qp > 9999 && queue->qp < 9999){
-			limit = queue->qp;
-			qp = 0;
-		}
 		if (!queue->queue[qp]) break;
-		if (queue->queue[qp]->type == desiredType) {
+		if (queue->queue[qp]->type == desiredType){
 			qp++;
 			return queue->queue[qp - 1];
+		}
+		if (qp >= 9999){
+			limit = (queue->qp != 0) ? queue->qp : limit;
+			qp = 0;
 		}
 	}
 	return nullptr;
